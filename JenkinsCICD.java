7e97bdfc4350f4ce83a800c7a1107270a09a083b
akash-45-ak <akashkalshetti45@outlook.com>
2024-09-04 08:20:37 +0530

    Testing Java Program Using Jenkins CI CD

--- JenkinsCICD.java
+++ JenkinsCICD.java
@@ -1,5 +1,5 @@
 public class JenkinsCICD{
     public static void main(String [] args){
-        System.out.println("CI CD JAVA Test Using Jenkins...")
+        System.out.println("CI CD JAVA Test Using Jenkins...");
     }
 }
